4d0a70ff94e272b311e23d104940115e45393bfa
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2024-08-24 20:15:36 +0530

    restrict first-timers to visit dashboard

--- web/app/dashboard/onboarding/page.tsx
+++ web/app/dashboard/onboarding/page.tsx
@@ -4,22 +4,22 @@ import OnboardingForm from "./_components/onboarding-form";
 import { auth } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
 
-async function shouldRedirectToOnboarding(userId: string) {
+async function shouldRedirectToDashboard(userId: string) {
   const merchant = await prisma.merchant.findFirst({
     where: {
       id: userId,
     },
   });
 
-  return !merchant;
+  return merchant;
 }
 
 const page = async () => {
   const { userId } = auth();
-  (await shouldRedirectToOnboarding(userId!)) && redirect("/dashboard/onboarding");
+  (await shouldRedirectToDashboard(userId!)) && redirect("/dashboard");
 
   return (
-    <div className="p-14 px-40">
+    <div className="p-14 px-40 h-full">
       <h2 className="text-3xl">Welcome to our onboarding process 🎁</h2>
       <h4 className="text-xl text-gray-500">
         You can change these later through the settings.
