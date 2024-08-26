a2b2f749d9331d40ef6e2d2c3e4d1ec7a2c7d190
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2024-08-26 14:56:18 +0530

    completed smart contract for payment pages

--- programs/pages/src/lib.rs
+++ programs/pages/src/lib.rs
@@ -6,54 +6,59 @@ declare_id!("Due8TAGvBMeKWY8XaFXVnLgQZj5FdVbKUVUrWDyQU2ye");
 pub mod pages {
     use super::*;
  
-    // Instruction to initialize a new pages account
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        // Reference to the pages account from the Initialize struct
-        let pages_account = &ctx.accounts.pages;
+    pub fn initialize_page(ctx: Context<Initialize>, title: String, description: String, price: f32, reward: String, image: String) -> Result<()> {
+        let pages_account = &mut ctx.accounts.pages;
+
+        pages_account.title = title;
+        pages_account.description = description;
+        pages_account.price = price;
+        pages_account.reward = reward;
+        pages_account.image = image;
+        
+
         msg!("Pages account created!");
         Ok(())
     }
- 
-    // Instruction to increment a pages account
-    pub fn increment(ctx: Context<Increment>, title: String) -> Result<()> {
-        // Mutable reference to the pages account from the Increment struct
+
+    pub fn update_page(ctx: Context<Update>, title: String, description: String, price: f32, image: String) -> Result<()> {
         let pages_account = &mut ctx.accounts.pages;
+
         pages_account.title = title;
+        pages_account.description = description;
+        pages_account.price = price;
+        pages_account.image = image;
         
-        // Accept data from rpc call and put on account
-        // pages_account.title = title;
- 
-        // msg!("Pages account updated! Current title: {}", pages_account.title);
+
+        msg!("Pages account updated!");
         Ok(())
     }
 }
  
-// Accounts required by the initialize instruction
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    // The account paying to create the counter account
     #[account(mut)]
-    pub user: Signer<'info>, // specify account must be signer on the transaction
+    pub user: Signer<'info>,
  
-    // The counter account being created and initialized in the instruction
     #[account(
-        init,         // specifies we are creating this account
-        payer = user, // specifies account paying for the creation of the account
-        space = 8 + 8 + 32 + 32 + 32 + 32 + 32 // space allocated to the new account (8 byte discriminator + 8 byte for u64)
+        init,
+        payer = user,
+        space = 8 + 8 + 32 + 32 + 32 + 32 + 32
     )]
-    pub pages: Account<'info, Pages>, // specify account is 'Pages' type
-    pub system_program: Program<'info, System>, // specify account must be System Program
+    pub pages: Account<'info, Pages>,
+    pub system_program: Program<'info, System>,
 }
- 
-// Account required by the increment instruction
+
 #[derive(Accounts)]
-pub struct Increment<'info> {
-    #[account(mut)] // specify account is mutable because we are updating its data
-    pub pages: Account<'info, Pages>, // specify account is 'Pages' type
+pub struct Update<'info> {
+    #[account(mut)]
+    pub pages: Account<'info, Pages>,
 }
  
-// Define structure of `Pages` account
 #[account]
 pub struct Pages {
-    title: String, // define title value type as String
+    title: String,
+    description: String,
+    price: f32,
+    reward: String,
+    image: String,
 }
