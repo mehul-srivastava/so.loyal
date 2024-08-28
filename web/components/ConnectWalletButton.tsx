983b299a136ddabaceb39257d6a2a4365a5cbc8a
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2024-08-28 10:07:36 +0530

    slight modification in wallet connect button

--- web/components/ConnectWalletButton.tsx
+++ web/components/ConnectWalletButton.tsx
@@ -9,22 +9,23 @@ import { useWallet } from "@solana/wallet-adapter-react";
 // Options for buttons: WalletConnectButton, WalletDisconnectButton, WalletMultiButton, WalletModalButton
 
 const ReactUIWalletConnectButtonDynamic = dynamic(
-  async () => (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
-  { ssr: false }
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+  { ssr: false },
 );
 
 const ReactUIWalletDisconnectButtonDynamic = dynamic(
-  async () => (await import("@solana/wallet-adapter-react-ui")).WalletDisconnectButton,
-  { ssr: false }
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletDisconnectButton,
+  { ssr: false },
 );
 
 export const ConnectThirdPartyWalletButton = () => {
   return (
     <ReactUIWalletConnectButtonDynamic
       style={{
-        background: "#fff",
-        color: "#000",
-        padding: "4px",
+        background: "#000",
+        color: "#fff",
       }}
     />
   );
@@ -40,7 +41,11 @@ interface Web3WalletButtonProps {
   onClickFn?: React.MouseEventHandler<HTMLButtonElement> | undefined;
 }
 
-export const Web3WalletButton = ({ action, text, onClickFn }: Web3WalletButtonProps) => {
+export const Web3WalletButton = ({
+  action,
+  text,
+  onClickFn,
+}: Web3WalletButtonProps) => {
   const { setVisible } = useWalletModal();
   const { disconnect } = useWallet();
 
@@ -55,7 +60,7 @@ export const Web3WalletButton = ({ action, text, onClickFn }: Web3WalletButtonPr
   return (
     <Button
       variant="link"
-      className="text-white px-0 font-normal text-xs"
+      className="px-0 text-xs font-normal text-white"
       onClick={(e) => handler(e)}
     >
       {text}
